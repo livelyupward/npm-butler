d398978666d76f11a45e61e732f1f3361f9002b4
Lively Upward <dave@livelyupward.dev>
2025-06-18 08:10:24 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env -S node --no-warnings
+
+import fs from "fs";
+import path from "path";
+import inquirer from "inquirer";
+import { execa } from "execa";
+import chalk from "chalk";
+
+const COMMON_COMMANDS = ["install", "update", "audit", "start"];
+
+const getPackageScripts = () => {
+  const pkgPath = path.join(process.cwd(), "package.json");
+
+  if (!fs.existsSync(pkgPath)) {
+    console.log(chalk.red("No package.json found in this directory."));
+    return [];
+  }
+
+  const pkg = JSON.parse(fs.readFileSync(pkgPath, "utf-8"));
+  const scripts = pkg.scripts || {};
+  return Object.keys(scripts);
+};
+
+(async () => {
+  const scriptChoices = getPackageScripts().map((s) => ({
+    name: `npm run ${s}`,
+    value: `run ${s}`,
+  }));
+
+  const commonChoices = COMMON_COMMANDS.map((cmd) => ({
+    name: `npm ${cmd}`,
+    value: cmd,
+  }));
+
+  const choices = [
+    ...scriptChoices,
+    new inquirer.Separator("── Common Commands ──"),
+    ...commonChoices,
+  ];
+
+  const { command } = await inquirer.prompt([
+    {
+      name: "command",
+      type: "list",
+      message: "Select an npm command to run:",
+      choices,
+      loop: false,
+    },
+  ]);
+
+  const parts = command.split(" ");
+  const cmd = "npm";
+  const args = parts;
+
+  console.log(chalk.green(`\nRunning: npm ${args.join(" ")}\n`));
+  const subprocess = execa(cmd, args, { stdio: "inherit" });
+
+  subprocess.stdout?.pipe(process.stdout);
+  subprocess.stderr?.pipe(process.stderr);
+})();
+
+export default getPackageScripts;
